6d62fff7be0647642bd52e1b3a8e03997b31f183
privateinternetdownload <srhtrjkyukuku@outlook.com>
2026-08-01 14:50:03 +0530

    Initialize Sphinx documentation configuration

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,15 @@
+project = 'private internet access download for linuxVPN'
+author = 'Your Name'
+release = '1.0'
 
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+html_js_files = [
+    'chatbot.js',
+]
+html_favicon = '.png'
